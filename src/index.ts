54ed366c6b4b029087cacafaa5bce628f35e5410
Md-Romjan-Ali <mdromjanali224512@gmail.com>
2026-08-10 08:06:53 +0600

    create get and get data by email mathod

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { setServers } from "node:dns";
 setServers(["8.8.8.8", "8.8.4.4"]);
 
-import { Application } from "express";
+import { Application, Request, Response } from "express";
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
@@ -25,9 +25,29 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         await client.connect();
-        const db = client.db('dohps')
-        const successCollection = db.collection('success')
-
+        const db = client.db('task-submit')
+        const taskSubmitCollection = db.collection('task')
+        // task
+        app.post('/api/posttask', async (req, res) => {
+            const corsur = req.body;
+            console.log(corsur);
+            const result = await taskSubmitCollection.insertOne(corsur)
+            res.send(result)
+        })
+        app.get('/api/getsubmit', async (req, res) => {
+            const result = await taskSubmitCollection.find().toArray()
+            res.send(result)
+        })
+        app.get('/api/getsubmitbyemail', async (req: Request, res: Response) => {
+            const query: {
+                email?: string
+            } = {}
+            if (typeof req.query.email === 'string') {
+                query.email = req.query.email;
+            }
+            const result = await taskSubmitCollection.find(query).toArray()
+            res.send(result)
+        })
         // await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
